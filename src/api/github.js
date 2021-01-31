ec5386c10fc2b9e1d6a73425d6ec9d040eb88eb0
sarsky123 <vikinglo123@gmail.com>
2021-01-31 21:35:23 +0800

    minor polishment

--- src/api/github.js
+++ src/api/github.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
-const BASE_URL = "https://api.github.com/";
+const _BASE_URL = "https://api.github.com/";
 
-const API_PATH = {
+const _API_PATH = {
   userRepo: userName => {
     return `users/${userName}/repos`;
   }
 };
 
-const githubAPIInstance = axios.create({
-  baseURL: BASE_URL
+const _githubAPIInstance = axios.create({
+  baseURL: _BASE_URL
 });
 
 export async function getUserRepo(userName, config, callback) {
-  let url = `${API_PATH.userRepo(userName)}`;
+  let url = `${_API_PATH.userRepo(userName)}`;
   let defaultConfig = {
     // recommended config
     accept: "application/vnd.github.v3+json"
   };
   config = Object.assign({}, defaultConfig, config);
-  await githubAPIInstance.get(url, config).then(response => {
+  await _githubAPIInstance.get(url, config).then(response => {
     callback && callback(response);
   });
 }
--- src/util/intersectionObserver.js
+++ src/util/intersectionObserver.js
@@ -1,10 +1,11 @@
+const _defaultOptions = {
+  root: null,
+  rootMargins: "10px",
+  threshold: 0.5
+};
+
 export default function(handler, optionArg, observingDOM) {
-  const defaultOptions = {
-    root: null,
-    rootMargins: "0px",
-    threshold: 0.5
-  };
-  let options = Object.assign({}, defaultOptions, optionArg);
+  let options = Object.assign({}, _defaultOptions, optionArg);
   const interSectionHandler = function(entries) {
     if (entries[0].isIntersecting) {
       handler();
